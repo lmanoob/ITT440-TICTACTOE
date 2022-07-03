6c334887933a37ebee1246a08b1c204109bfc7fe
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-07-03 20:24:10 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,47 +1,68 @@
-import socket
-import sys
+import socket            
+import os
+import signal
 
-#Create socket object
-try:
-	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-	print("Could not create socket")
+WIN_COMBINATIONS = [(0, 1, 2),(3, 4, 5),(6, 7, 8),(0, 3, 6),(1, 4, 7),(2, 5, 8),(0, 4, 8),(2, 4, 6)]
 
-#Use host that user input
-script,host = sys.argv
+s = socket.socket()        
+ 
 
-#Connect to a remote socket at specified host address
-socket.connect((host, 6869))
-#Initial response
-response="WAIT"
+s.connect(('192.168.50.164', 12345))
+next = None
 
-"""Client loop. Always either waiting for further instructions and not taking user input
-or taking user input and returning server response"""
+
+def win(places):
+    if (places != "---------"):
+        for placeA, placeB, placeC in WIN_COMBINATIONS:
+            if places[placeA] == places[placeB] == places[placeC] and (places[placeA] != '-' and places[placeB] != '-' and places[placeC] != '-'):
+                s.send("WIN".encode())
+                return True
+
+    return False
+
+def game(places):
+    # the game board
+    print( places[0] + " | " + places[1] + " | " + places[2])
+    print( places[3] + " | " + places[4] + " | " + places[5])
+    print( places[6] + " | " + places[7] + " | " + places[8])
+
+ 
 while True:
+    while next == None:
+        places = s.recv(1024).decode()
 
-	while "WAIT" in response:
-		response=socket.recv(4096)
-		print response
+        if places == "WIN":
+            print("Player 2 win! Game ending")
+            s.close()
+            quit()
+        else:
+            places = places[:9]
 
-	#Take user input from command line interface
-	userInput=raw_input("ttt->")
-	if(userInput==''):
-		continue
+        print("Received response from Player 2")
+        game(places)
 
-	#Send user input to server, and collect response
-	socket.send(userInput)
-	response=socket.recv(4096)
+        break
+    
+    while True:
+        next = int(input("Where do you want to place an O? (1,2,3,4,5,6,7,8,9)")) - 1
+        placeslist = list(places)
+        if (placeslist[next] == '-'):
+            placeslist[next] = 'O'
+            places = ''.join(placeslist)
+            break
+        else:
+            print("this placement is filled already")
+            continue
 
-	#Error handling
-	if response=="400 ERR":
-		print "Invalid command"
+    os.system("clear")
 
-	#If response is valid, print it
-	else:
-		print response
+    if (win(places)):
+        game(places)
+        print("You win! Game ending")
+        s.close()
+        quit()
 
-		#Exit loop and quit program if user chooses to exit
-		if response=="DISC":
-			sys.exit()
+    s.send(places.encode())
+    next = None
 
-socket.close()
+    # s.close() 
