a46c8e643788308136f0323abb50086b5decadc8
lmanoob <103398600+lmanoob@users.noreply.github.com>
2022-07-03 20:24:52 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,212 +1,79 @@
-import socket
+import socket      
 import sys
-import Queue
-from thread import *
-from threading import RLock
-from player import Player
-from tttgame import TTTGame
-import time
-import uuid
+import os
+import signal
 
-#Create server socket object
-try:
-	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error:
-	print('Could not create socket')
+WIN_COMBINATIONS = [(0, 1, 2),(3, 4, 5),(6, 7, 8),(0, 3, 6),(1, 4, 7),(2, 5, 8),(0, 4, 8),(2, 4, 6)]
 
-#Bind to port 6869
-try:
-    socket.bind(('', 6869))
-except socket.error:
-    print ('Failed')
-    sys.exit()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #create socket
 
-socket.listen(5)
-print 'Listening'
 
-#Keeps track of players currently in the server
-players=[]
-#Keeps track of games currently underway. Stores UUID objects.
-gameList=[]
-lock=RLock()
+s.bind(('192.168.50.164', 12345))     
+next = None #players move  
+init = True #detects if it the first round so it won't repeat
+places = "---------"
+print ("Game active at 12345")
 
-def connect(clientSock):
-	"""Run for each client that connects to the server
-	Handles messages sent from the client and sends corresponding responses"""
+s.listen(5)    
+print ("Listening for clients")
+c, addr = s.accept()
 
-	#Opening message to client. Also breaks initial client WAIT loop, allowing the client to send input
-	clientSock.send("Welcome to Tic Tac Toe")
+def win(places):
+    for placeA, placeB, placeC in WIN_COMBINATIONS: #this is the win combination placements
+        if places[placeA] == places[placeB] == places[placeC] and (places[placeA] != '-' and places[placeB] != '-' and places[placeC] != '-'):
+            c.send("WIN".encode()) # if the player wins then this message prompt appear
+            return True
 
-	#Main connection loop. Handles all messages from client
-	while True:
-		#Command sent by client
-		cmd=clientSock.recv(4096)
-		#Login functionality
-		req=cmd.split(" ")
-		#Checks that user is asking to login, and also has specified a username
-		if req[0]=='login' and req[1]:
-			username=req[1]
-			found = False
-			#Check if username exists already
-			for p in players:
-				#If it does, then the user logged back in. No need to create a new user
-				if p.username==username:
-					currPlayer=p
-					found=True
-					print currPlayer.username+" logged in"
+    return False
 
-			#Add a new player if no user with specified username exists
-			if not found:
-				lock.acquire()
-				#Create player object with this thread's client socket and the user specified username
-				currPlayer = Player(username,clientSock)
-				players.append(currPlayer)
-				print currPlayer.username+ " created an account and logged in"
-				lock.release()
+def game(places):
+    #  the game board
+    print( places[0] + " | " + places[1] + " | " + places[2])
+    print( places[3] + " | " + places[4] + " | " + places[5])
+    print( places[6] + " | " + places[7] + " | " + places[8])
 
-			clientSock.send("Welcome: "+currPlayer.username)
-
-		#Play functionality
-		elif req[0]=='play':
-			#Make player wait to find an opponent if none is available
-			currPlayer.setAvailable()
-			opponent=None
-			while opponent is None:
-				for opp in players:
-					if opp.username!=currPlayer.username and opp.state=="available":
-						opponent=opp
-
-				#Use sleep function to wait until opponent is found
-				if opponent is None:
-					time.sleep(1)
-				#Once an opponent is found, generate a gameID, add it to the list of current games,
-				#and create a game with the found opponent and the current player
-				else:
-					lock.acquire()
-					id=uuid.uuid4()
-					game=TTTGame(opponent, currPlayer, id)
-					print "New Game with id: "+str(id)
-					gameList.append(game.gameID)
-					currPlayer.setBusy()
-					opponent.setBusy()
-					lock.release()
-
-					#See below for startGame documentation
-					startGame(game)
-
-					#Once the game is finished, removethe game from the list of current games
-					#and set the two player's states back to loggedin
-					lock.acquire()
-					gameList.remove(id)
-					currPlayer.setLoggedIn()
-					opponent.setLoggedIn()
-					lock.release()
-
-		#List all available games
-		elif cmd=="games":
-			if len(gameList)>0:
-				listOfGames=""
-				for g in gameList:
-					listOfGames+=str(g)
-					listOfGames+="\n"
-				clientSock.send(listOfGames)
-			else:
-				clientSock.send("No games currently")
-
-		#List all players logged 
-		elif cmd=="who":
-			if len(players)>0:
-				listOfPlayers=""
-				for p in players:
-					listOfPlayers+=str(p.username+"\n")
-				clientSock.send(listOfPlayers)
-			else:
-				clientSock.send("No players currently")
-
-		#Help functionality
-		elif cmd=='help':
-			clientSock.send("Commands: "
-				+"\nlogin username-Enter login followed by a username of your choice."
-				+"\nplay-Finds and starts a game, or waits until a game is found. Must be logged in to play"
-				+"\nplace n-Move to position n, where n is between 0 and 8. Must be logged in and in a game to use this command."
-				+"\ngames-Show all games currently going on."
-				+"\nwho- Show all players currently logged in or playing a game"
-				+"\nexit-Leave the server.")
-
-		#Exit functionality	
-		elif cmd=='exit':
-			print'Client disconnected'
-			clientSock.send("DISC")
-			clientSock.close()
-			break
-
-		else: 
-			clientSock.send("400 ERR")
-
-def startGame(game):
-	"""Contains the main game loop
-	Takes a TTTGame object as an argument"""
-
-	gameover=False
-
-	#Main game loop
-	while not gameover:
-		game.turn.send("Your turn")
-		game.waiting.send("WAIT Other players turn")
-		move=game.turn.conn.recv(4096)
-		if checkMove(game,move):
-			response=game.makeMove(move.split(" ")[1])
-
-			#Case: Move is valid but not game ending
-			#Give both players an update of the game board and switch turns. Continue with game loop.
-			if response=="301 NPT":
-				game.turn.send("WAIT"+game.drawBoard())
-				game.waiting.send("WAIT"+game.drawBoard())
-				game.changeTurn()
-
-			#Case: Game finished and no winner.
-			#Notify players that it is a draw and exit game loop
-			elif response=="300 FIN":
-				gameover=True
-				game.turn.send("No winner")
-				game.waiting.send("No winner")
-
-			#Case: Game finished and winner
-			#Notify players who won and exit game loop
-			elif response=="300 WIN":
-				gameover=True
-				game.turn.send("Game over. You won!")
-				game.waiting.send("Game over. "+game.turn.username+" won!")
-				print "Game over"
-		else:
-			continue
-	return
-
-def checkMove(game, move):
-	"""Takes a player move as argument
-	and ensures that the move is valid."""
-	try:
-		statement, pos=move.split(" ")
-		pos=int(pos)
-	except ValueError:
-		return False
-
-	#First check that the user has properly requested to place
-	if statement !="place":
-		return False
-
-	#Then check that the user's choice is a valid board position
-	elif pos<0 or pos>8 or game.board[pos]=='X' or game.board[pos]=='O':
-		return False
-
-	else:
-		return True
-
-#Main loop
 while True:
-	clientSock, addr=socket.accept()
-	print 'Connected to a client'
+    if (init):
+        c.send(places.encode())#send placements made from the player to the other
+        init = False
 
-	start_new_thread(connect, (clientSock,))
+   
+    while next == None:
+        places = c.recv(1024).decode()# receive the placements from the other player
+            
+        if places == "WIN":
+            print("Player 1 win! Game ending")
+            s.close()
+            raise  SystemExit
+        else:
+            places = places[:9]
+                
+        print("Received response from Player 1")
+        game(places)
 
-socket.close()
+        break
+
+    while True:
+        next = int(input("Where do you want to place an X? (1,2,3,4,5,6,7,8,9)")) - 1
+        placeslist = list(places)
+        if (placeslist[next] == '-'):#this is for the programming to detect if the spot is empty or not
+            placeslist[next] = 'X'
+            places = ''.join(placeslist)
+            break
+        else:
+            print("This placement is filled already")#to tell the players when someone put the thing inside the space
+            continue
+
+    os.system("clear")#clear the plane so it's easier to see
+
+    if (win(places)):
+        game(places)
+        print("You win! Game ending")
+        s.close()
+        raise  SystemExit
+
+    c.send(places.encode())
+    next = None
+
+    # c.close()
